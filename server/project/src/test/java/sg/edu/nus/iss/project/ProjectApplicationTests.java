b99bf8895a55670d468dea8ae843b406d990fb11
David Yap <davvyap@gmail.com>
2023-06-05 12:23:46 +0800

    initiate server

---
+++ server/project/src/test/java/sg/edu/nus/iss/project/ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
